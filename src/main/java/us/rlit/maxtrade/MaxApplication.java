d72e52db2c8ac9febb27b6cefa061a121f8745c9
rDevelop <roblitvin@me.com>
2017-05-22 06:16:33 -0500

    initial commit

---
+++ src/main/java/us/rlit/maxtrade/MaxApplication.java
@@ -0,0 +1,15 @@
+package us.rlit.maxtrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by rob on 5/22/17.
+ */
+@SpringBootApplication
+public class MaxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaxApplication.class, args);
+    }
+}
